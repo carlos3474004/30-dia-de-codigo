c4faabeb33a3fabfb623c5c24ef7456bcbdf1f43
carlos3474004 <cg3474004@gmail.com>
2026-07-14 16:04:44 +0000

    day2

---
+++ hackerrank/day2/main.py
@@ -0,0 +1,28 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    comida = meal_cost
+    propina = meal_cost * (tip_percent/100)
+    inpuesto = meal_cost * (tax_percent/100)
+
+    total = comida + propina + inpuesto
+    
+    print(f'total a pagar: {round(total)}')
+    
+if __name__ == '__main__':
+    meal_cost = float(input("comida: ").strip())
+
+    tip_percent = int(input("propina: ").strip())
+
+    tax_percent = int(input("inpuesto: ").strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
